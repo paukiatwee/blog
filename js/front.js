72b69c9b45200e08c520b7a7375c69e891072fa4
Pau Kiat Wee <paukiatwee@gmail.com>
2012-07-28 22:05:26 +0800

    added simple line graph for aws

--- js/front.js
+++ js/front.js
@@ -16,31 +16,21 @@ $(function() {
 function drawCharts() {
   $(".chart").each(function() {
     $.getJSON($(this).data("remote"), function(json) {
-      // var raw = [
-      //     ['Month', 'On Demand', 'Reserved - Light'],
-      //     ['2004',  1000,      400],
-      //     ['2005',  1170,      460],
-      //     ['2006',  660,       1120],
-      //     ['2007',  1030,      540]
-      //   ];
-      // // console.log(json.rows.grade)
-      // var data = google.visualization.arrayToDataTable(raw);
-      var data = new google.visualization.DataTable();
-      for(var column in json.columns) {
-        data.addColumn(json.columns[column].type, json.columns[column].label);
-      }
-      for(var row in json.rows) {
-        if(json.rows[row].grade === 'on-demand') {
-          
-        console.log(json.rows[row].type);
-        console.log(json.rows[row].price);
-        data.addRow([json.rows[row].type, json.rows[row].price]);
-        }
-      }
+
+      var raw = [
+          ['Month', 'On Demand', 'Reserved - Light', 'Reserved - Medium', 'Reserved - Heavy', 'Sport Instance'],
+          ['0',  0,     json.aml, json.amm, json.amh, 0],
+          ['2',  json.am_d * 750 * 2, json.am_l * 750 * 2 + json.aml, json.am_m * 750 * 2 + json.amm, json.am_h * 750 * 2 + json.amh, json.am_s * 750 * 2],
+          ['4',  json.am_d * 750 * 4, json.am_l * 750 * 4 + json.aml, json.am_m * 750 * 4 + json.amm, json.am_h * 750 * 4 + json.amh, json.am_s * 750 * 4],
+          ['6',  json.am_d * 750 * 6, json.am_l * 750 * 6 + json.aml, json.am_m * 750 * 6 + json.amm, json.am_h * 750 * 6 + json.amh, json.am_s * 750 * 6],
+          ['8',  json.am_d * 750 * 8, json.am_l * 750 * 8 + json.aml, json.am_m * 750 * 8 + json.amm, json.am_h * 750 * 8 + json.amh, json.am_s * 750 * 8],
+          ['10',  json.am_d * 750 * 10, json.am_l * 750 * 10 + json.aml, json.am_m * 750 * 10 + json.amm, json.am_h * 750 * 10 + json.amh, json.am_s * 750 * 10],
+          ['12',  json.am_d * 750 * 12, json.am_l * 750 * 12 + json.aml, json.am_m * 750 * 12 + json.amm, json.am_h * 750 * 12 + json.amh, json.am_s * 750 * 12]
+        ];
+
+      var data = google.visualization.arrayToDataTable(raw);
       // Set chart options
-      var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':730,
-                       'height':350};
+      var options = {'width':730, 'height':500};
       var chart = new google.visualization.LineChart(document.getElementById('chart'));
       chart.draw(data, options);
     });
